cac25e0d8063bb39e7b907c8d8445f892b5f71bf
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-09-21 20:59:14 +0300

    check if game ok

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 
 let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '' ];
+let boardNumber =3;
 
-for(let i = numbers.length-1; i > 0; i--){
-    const j = Math.floor(Math.random() * i)
-    const temp = numbers[i]
-    numbers[i] = numbers[j]
-    numbers[j] = temp
-  }
-
+function shuffle(){
+    for(let i = numbers.length-1; i > 0; i--){
+        const j = Math.floor(Math.random() * i)
+        const temp = numbers[i]
+        numbers[i] = numbers[j]
+        numbers[j] = temp
+      } 
+}
+let boardNumbers = numbers;
 
 function AddAllCards(){
     document.getElementById('board').innerHTML = "";
@@ -24,6 +27,53 @@ function AddCard(number) {
 </div>`
 
     document.getElementById('board').innerHTML += html;
+}
+
+function GetCounter()
+{
+    let counter =0;
+    boardNumbers.forEach(number => {
+        for(let i = boardNumbers.indexOf(number) +1 ; i < boardNumbers.length; i++){
+            if(number > boardNumbers[i] &&  boardNumbers[i] != "")
+            {
+                counter++;
+            }
+          }
+    });
+
+    return counter;
+}
+
+function CheckIfGameCorrect()
+{
+    let counter  = GetCounter();
+
+    if(boardNumber %2 == 0)
+    {
+       let place =  numbers.indexOf("") / boardNumber +1 ;
+       if(place+ counter%2==0)
+        {
+           return true;
+        }
+        return false;
+    } 
+    else
+    {
+        if(counter%2==0)
+        {
+            return true;
+        }
+        return false;
+    } 
+}
+
+
+shuffle();
+
+while(!CheckIfGameCorrect())
+{
+    console.log("board not ok")
+    shuffle();
 }
 
 AddAllCards();
