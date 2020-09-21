a4f400d91f3f4a8caa936d7d50dda1d459d3f561
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-09-21 22:23:56 +0300

    check place of number

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function AddAllCards(){
 }
 
 function AddCard(number) {
-    var html = `<div class="col-sm-3 border d-flex justify-content-center text-center Card" onclick="MoveNumber(this)">
+    var html = `<div class="col-sm-3 d-flex justify-content-center text-center Card" onclick="MoveNumber(this)">
     <div>
     <p class="number">${number}</p>
     </div>
@@ -64,34 +64,109 @@ function CheckIfGameCorrect()
             return true;
         }
         return false;
-    } 
+    }
 }
 
 
 shuffle();
 
-while(!CheckIfGameCorrect())
+// while(!CheckIfGameCorrect())
+// {
+//     console.log("board not ok")
+//     shuffle();
+// }
+if(!CheckIfGameCorrect())
 {
     console.log("board not ok")
-    shuffle();
 }
 
 AddAllCards();
 
 function MoveNumber(card)
 {
+    //just if it ok can move
     let p =card.querySelectorAll("p")[0];
     let number = p.innerText;
-    console.log(number);
-    console.log(document.getElementsByClassName("number"));
+    let cardPlace=0;
+    for (let index = 0; index < document.getElementsByClassName("number").length; index++) {
+        if( document.getElementsByClassName("number")[index].innerText == number)
+        {
+             cardPlace =index;
+        }
+        
+    }
+   
     for (let index = 0; index < document.getElementsByClassName("number").length; index++) {
         console.log(document.getElementsByClassName("number")[index].innerText);
         if( document.getElementsByClassName("number")[index].innerText == "")
         {
             console.log("enter");
-            document.getElementsByClassName("number")[index].innerHTML = number;
+            console.log(index+" "+cardPlace);
+            if(index%3==0)
+            {
+                if ((index+1)==cardPlace|| (index-3)==cardPlace ||(index+3==cardPlace))
+                {
+                    console.log("enter 2")
+                    document.getElementsByClassName("number")[index].innerHTML = number;
+                    p.innerHTML = "";
+                }
+
+            }
+            else if(index+1%3==0)
+            {
+                if ((index-1)==cardPlace || (index-3)==cardPlace ||(index+3==cardPlace))
+            {
+                console.log("enter 2")
+                document.getElementsByClassName("number")[index].innerHTML = number;
+                p.innerHTML = "";
+            }
+
+            }
+            else
+            {
+                if ((index+1)==cardPlace || (index-1)==cardPlace || (index-3)==cardPlace ||(index+3==cardPlace))
+                {
+                    console.log("enter 2")
+                    document.getElementsByClassName("number")[index].innerHTML = number;
+                    p.innerHTML = "";
+                }
+            } 
         }
     }
- 
-    p.innerHTML = "";
+    IsWinner();
+}
+
+function AddColorWhenCorrect(element)
+{
+    element.classList.add("addColor");
+}
+function RemoveColor(element)
+{
+    element.classList.remove("addColor");
+}
+
+
+
+function IsWinner()
+{
+    let card = document.getElementsByClassName("Card");
+    let count =0;
+    for (let index = 0; index < card.length; index++) {
+       if(index+1 == card[index].getElementsByClassName("number")[0].innerText)
+       {
+        count++;
+        AddColorWhenCorrect(card[index]);
+       }
+       else
+       {
+        RemoveColor(card[index]);
+       }
+    }
+
+    if(count==card.length-1)
+    {
+        document.getElementById("Winner").innerHTML="You Win!";
+        console.log("winner");
+    }
+
 }
